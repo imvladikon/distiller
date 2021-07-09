665db92cb7144549deba11e1ebda292a64c39329
Vladimir Gurevich <imvladikon@gmail.com>
2021-07-10 01:29:57 +0300

    init

--- pipelines/text_multiclassification_pipeline.py
+++ pipelines/text_multiclassification_pipeline.py
@@ -4,20 +4,23 @@ from transformers import Pipeline
 
 class TextClassificationPipeline(Pipeline):
 
-    def __init__(self, return_all_scores: bool = False, **kwargs):
+    def __init__(self, return_all_scores: bool = False, is_multilabel: bool = True, **kwargs):
         super().__init__(**kwargs)
 
         self.return_all_scores = return_all_scores
+        self.is_multilabel = is_multilabel
 
     def __call__(self, *args, **kwargs):
         outputs = super().__call__(*args, **kwargs)
 
         if self.model.config.num_labels == 1:
             scores = 1.0 / (1.0 + np.exp(-outputs))
+        elif self.is_multilabel:
+            scores = outputs
         else:
-            # TODO: fix for multilabel multiclassification
             scores = np.exp(outputs) / np.exp(outputs).sum(-1, keepdims=True)
-        if self.return_all_scores:
+
+        if self.return_all_scores or self.is_multilabel:
             return [
                 [{"label": self.model.config.id2label[i], "score": score.item()} for i, score in enumerate(item)]
                 for item in scores
@@ -33,27 +36,28 @@ if __name__ == '__main__':
     from pprint import pprint
     from modeling.bert_multilabel_classification import BertForMultiLabelSequenceClassification
 
-
-    labels = ['contract',
-              'ooo',
-              'payments',
-              'pi',
-              'pricing',
-              'reject',
-              'reply',
-              'scheduling']
+    labels = [
+        "reject",
+        "scheduling",
+        "pricing",
+        "pi",
+        "ooo",
+        "payments",
+        "contract",
+        "reply"
+    ]
 
     id2label = {k: v for k, v in enumerate(labels)}
     label2id = {v: k for k, v in id2label.items()}
 
-    model_checkpoint = "../models/tuned/tuned_final_model_8_tag_case_sensitive"
+    model_checkpoint = "../models/tuned/tuned_bertreply"
     model = BertForMultiLabelSequenceClassification.from_pretrained(model_checkpoint, num_labels=len(labels))
     model.config.label2id = label2id
     model.config.id2label = id2label
 
     pipe = TextClassificationPipeline(
         model=model,
-        tokenizer= AutoTokenizer.from_pretrained("bert-base-uncased"),
-        return_all_scores=True)
+        tokenizer=AutoTokenizer.from_pretrained("bert-base-uncased"),
+        is_multilabel=True)
     message = """Nice to meet you. I can still me tomorrow at 2pm for 30 minutes. I have another call at 2:30."""
     pprint(pipe(message))
