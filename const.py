ac7129e28e55fd01e7cd3b86634f23259bb5dd8a
Vladimir Gurevich <imvladikon@gmail.com>
2021-07-09 15:15:14 +0300

    init

--- const.py
+++ const.py
@@ -6,14 +6,16 @@ ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 random_state = 200
 
-labels = ['contract',
-          'ooo',
-          'payments',
-          'pi',
-          'pricing',
-          'reject',
-          'reply',
-          'scheduling']
+labels = [
+    "reject",
+    "scheduling",
+    "pricing",
+    "pi",
+    "ooo",
+    "payments",
+    "contract",
+    "reply"
+]
 
 id2label = {k: v for k, v in enumerate(labels)}
 label2id = {v: k for k, v in id2label.items()}
