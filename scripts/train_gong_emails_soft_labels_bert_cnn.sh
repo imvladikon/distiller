ebb66b03306c7f55847266e2c39caef63632f392
Vladimir Gurevich <imvladikon@gmail.com>
2021-07-17 08:27:27 +0300

    init

--- scripts/train_gong_emails_soft_labels_bert_cnn.sh
+++ scripts/train_gong_emails_soft_labels_bert_cnn.sh
@@ -12,7 +12,7 @@ cd $ROOT_DIR/models/gong_soft_labels &&
     --do_lower_case \
     --one_cycle_train \
     --tokenizer_name=bert-base-uncased \
-    --output_dir=$ROOT_DIR/models/gong_soft_labels/bert-base-uncased-ft \
+    --output_dir=$ROOT_DIR/models/gong_soft_labels/bert-cnn-base-uncased-ft \
     --train_batch_size=24 \
     --val_batch_size=12 \
     --gradient_accumulation_steps=2 \
